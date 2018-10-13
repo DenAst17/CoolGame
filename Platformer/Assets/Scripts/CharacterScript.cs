89d5554599756701d476db56dab3e727b7ae5eef
Kiuh <30412223+Kiuh@users.noreply.github.com>
2018-10-13 17:05:04 +0300

    Character Move

--- Platformer/Assets/Scripts/CharacterScript.cs
+++ Platformer/Assets/Scripts/CharacterScript.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private int lives = 5;
+    [SerializeField]
+    private float speed = 3.0f;
+    [SerializeField]
+    private float jumpForse = 62.0f;
+
+    private bool isGrounded;
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal")) Run();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        sprite.flipX = direction.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpForse, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = colliders.Length > 1;
+    }
 }
