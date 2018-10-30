a06353d442f983a345d198a9fda27c1ada730985
Kiuh <30412223+Kiuh@users.noreply.github.com>
2018-10-31 00:07:24 +0200

    MoveCoin

--- Platformer/Assets/Scripts/Coin.cs
+++ Platformer/Assets/Scripts/Coin.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Coin : MonoBehaviour {
 
     [SerializeField]
     public static int cost = 5;
-
+    [SerializeField]
+    public static float h = 2;
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x,transform.position.y+(float)Math.Sin(Time.time*Math.PI*1.5)/-100,transform.position.z);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "player")
