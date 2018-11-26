2762d5da27b759d6fb72369dfa79123fdb68ba1d
Kiuh <30412223+Kiuh@users.noreply.github.com>
2018-11-26 22:40:46 +0200

    Р+

---
+++ Platformer/Assets/Scripts/Water.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+    [SerializeField]
+    private float speed = 2;
+    [SerializeField]
+    private int border = 4;
+    [SerializeField]
+    private Vector3 dir;
+    private void Update()
+    {
+        Transform Tt = GetComponent<Transform>();
+        Tt.position = new Vector3(transform.position.x+speed*Time.deltaTime,transform.position.y+(float)Math.Sin(Time.time * Math.PI * 1.3) / -100, transform.position.z);
+    }
+}
