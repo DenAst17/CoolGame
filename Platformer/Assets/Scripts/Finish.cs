094e641168891a68bd47b1e7f4e20412b0f1a0b4
Kiuh <30412223+Kiuh@users.noreply.github.com>
2018-11-27 23:21:53 +0200

    Finish

--- Platformer/Assets/Scripts/Finish.cs
+++ Platformer/Assets/Scripts/Finish.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (FindObjectOfType<Builder>())
+        {
 
+        }
+        else
+        {
+            SceneManager.LoadScene("SelectLevel");
+        }
     }
 }
