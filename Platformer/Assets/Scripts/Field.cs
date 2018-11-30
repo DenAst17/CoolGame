ce54b9902ada2612b0927cf42ceb83d99a5c4e06
Kiuh <30412223+Kiuh@users.noreply.github.com>
2018-12-01 00:58:32 +0200

    Zombie+

--- Platformer/Assets/Scripts/Field.cs
+++ Platformer/Assets/Scripts/Field.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Field : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private int fieldspl = 1;
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + (float)Math.Sin(Time.time * Math.PI * 1.5) / -100, transform.position.z);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            if (Hero.fields + fieldspl >= 5)
+            {
+                Hero.fields = 5;
+            }
+            else
+            {
+                Hero.fields += fieldspl;
+            }
+            GameObject.Destroy(this.gameObject);
+        }
+    }
 }
---
+++ Platformer/Assets/Scripts/ZombieMale.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMale : MonoBehaviour {
+    private Rigidbody2D rb;
+    private SpriteRenderer sp;
+    private Animator anim;
+    private string fi = "Left";
+    private float speed = 0.04f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    private void Update()
+    {
+        anim.SetInteger("State", 1);
+        if (fi == "Right")
+        {
+            transform.position += Vector3.right * speed;
+            sp.flipX = false;
+        }
+        else
+        {
+            transform.position += Vector3.left * speed;
+            sp.flipX = true;
+        }
+        if (fi == "Right")
+        {
+            if (Rightup() == true || Rightdown() == false)
+            {
+                fi = "Left";
+            }
+        }
+        else
+        {
+            if (Leftup() == true || Leftdown() == false)
+            {
+                fi = "Right";
+            }
+        }
+    }
+    private bool Rightup()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x += 0.5f;
+        Vector2 dol; dol.x = 0.7f; dol.y = 0.2f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private bool Leftup()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x -= 0.5f;
+        Vector2 dol; dol.x = 0.7f; dol.y = 0.2f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private bool Leftdown()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x -= 0.5f; kol.y -= 1.5f;
+        Vector2 dol; dol.x = 0.7f; dol.y = 0.2f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private bool Rightdown()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x += 0.5f; kol.y -= 1.5f;
+        Vector2 dol; dol.x = 0.7f; dol.y = 0.2f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
