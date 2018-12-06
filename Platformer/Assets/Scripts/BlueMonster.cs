f270afd5921654510189d715b485716bbf1a172b
Kiuh <30412223+Kiuh@users.noreply.github.com>
2018-12-06 19:44:13 +0200

    NewMonster

---
+++ Platformer/Assets/Scripts/BlueMonster.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueMonster : MonoBehaviour {
+    Animator an;
+    Rigidbody2D rb;
+    SpriteRenderer sp;
+    private bool touch = true;
+    private string dir = "Right";
+    private float JumpImpuls = 35;
+    [SerializeField]
+    private float JumpImpulsel = 30;
+    [SerializeField]
+    private float JumpImpulser = 40;
+    private void Awake()
+    {
+        an = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+        an.SetInteger("State", 1);
+    }
+    private void Update()
+    {
+        if (Time.timeScale != 0)
+        {
+            if (Rightdown() == false && Leftdown() == false)
+            {
+                touch = false;
+            }
+            if (dir == "Right" && touch == true)
+            {
+                JumpImpuls = Random.Range(JumpImpulsel, JumpImpulser);
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(1,1) * JumpImpuls,ForceMode2D.Impulse);
+                sp.flipX = true;
+            }
+            else if(touch == true)
+            {
+                JumpImpuls = Random.Range(JumpImpulsel, JumpImpulser);
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(-1, 1) * JumpImpuls, ForceMode2D.Impulse);
+                sp.flipX = false;
+            }
+            if (dir == "Right")
+            {
+                if (Rightup() == true || Rightdown() == false)
+                {
+                    dir = "Left";
+                }
+            }
+            else
+            {
+                if (Leftup() == true || Leftdown() == false)
+                {
+                    dir = "Right";
+                }
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Time.timeScale != 0)
+        {
+            if (collision.gameObject.tag == "Solid")
+            {
+                touch = true;
+            }
+            if (collision.gameObject.tag == "Monster")
+            {
+                if (dir == "Right") { dir = "Left"; }
+                if (dir == "Left") { dir = "Right"; }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Kokol")
+        {
+            
+            if (dir == "Right")
+            {
+                dir = "Left"; JumpImpuls = Random.Range(JumpImpulsel, JumpImpulser);
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(1, 1) * JumpImpuls, ForceMode2D.Impulse);
+            }
+            if (dir == "Left")
+            {
+                dir = "Right"; JumpImpuls = Random.Range(JumpImpulsel, JumpImpulser);
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(-1, 1) * JumpImpuls, ForceMode2D.Impulse);
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (Time.timeScale != 0)
+        {
+            if (collision.gameObject.tag == "Solid")
+            {
+                touch = false;
+            }
+        }
+    }
+    private bool Rightup()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x += 0.5f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private bool Leftup()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x -= 0.5f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private bool Leftdown()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x -= 2f; kol.y -= 1.5f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private bool Rightdown()
+    {
+        int j = 0;
+        Vector2 kol = transform.position; kol.x += 2f; kol.y -= 1.5f;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(kol, 0.2f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject.tag == "Solid")
+            {
+                j++;
+            }
+        }
+        if (j > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
