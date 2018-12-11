bc1bd50e9e46c29d1c844237f861fde10e80fe7f
Kiuh <30412223+Kiuh@users.noreply.github.com>
2018-12-11 21:10:49 +0200

    Portal+

---
+++ Platformer/Assets/Scripts/Boosters/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+    [SerializeField]
+    private GameObject target;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            collision.gameObject.transform.position = target.transform.position;
+        }
+    }
+}
