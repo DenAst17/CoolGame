68c2c9fcc06e3771f9a47382d11c74b332d12ddb
DenAst17 <45298493+DenAst17@users.noreply.github.com>
2018-12-14 00:17:48 +0200

    Zagnali spat
    Но начал + сделал урон 100 при пасхальных настройках спайка.

--- Platformer/Assets/Scripts/Monsters/Spiky_orange.cs
+++ Platformer/Assets/Scripts/Monsters/Spiky_orange.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class Spiky_orange : MonoBehaviour
 {
+    [SerializeField]
+    private bool Easter_lvl;
     private int damage;
     private void Awake()
     {
-        switch (Global.difficulty)
-        {
-            case 1:
-                damage = 15;
-                break;
-            case 2:
-                damage = 20;
-                break;
-            case 3:
-                damage = 25;
-                break;
-            case 4:
-                damage = 30;
-                break;
-            case 5:
-                damage = 35;
-                break;
-        }
+        if (Easter_lvl)
+            damage = 100;
+        else
+            switch (Global.difficulty)
+            {
+                case 1:
+                    damage = 15;
+                    break;
+                case 2:
+                    damage = 20;
+                    break;
+                case 3:
+                    damage = 25;
+                    break;
+                case 4:
+                    damage = 30;
+                    break;
+                case 5:
+                    damage = 35;
+                    break;
+            }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
